b086073eb3b9c38c66a3f9f87136cbe9229f9121
AlexanderSatek <AlexanderSatek@deutschebahn.de>
2017-11-09 12:49:09 +0100

    update

---
+++ src/main/java/at/loup/cxftest/CxfTestApplication.java
@@ -0,0 +1,11 @@
+package at.loup.cxftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxfTestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CxfTestApplication.class, args);
+	}
+}
